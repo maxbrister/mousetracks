5d2c83089ea2d64488b93cfe8605a51027668e73
Max Brister <max@2bass.com>
2020-08-23 19:10:08 -0700

    Download 68 predictor

--- mousetracks.py
+++ mousetracks.py
@@ -1,17 +1,30 @@
+import bz2
 import cv2
 import dlib
 import numpy as np
 import tarfile
+import os
 import os.path
 import sys
 import urllib.request
 from os import path
 
-detector = dlib.get_frontal_face_detector()
+def download_predictor():
+    name = 'shape_predictor_68_face_landmarks.dat'
+    url = f'http://dlib.net/files/{name}.bz2'
+    dest = path.join('data', name)
+    if path.exists(dest):
+        return dest
 
-def download_detector():
-    url = 'http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2'
-    if path.exists('data/shape_predictor_68_face_landmarks.dat')
+    os.makedirs('data')
+    tmp_dest = dest + '.bz2'
+    urllib.request.urlretrieve(url, tmp_dest)
+    fin = bz2.open(tmp_dest)
+    data = fin.read()
+    with open(dest, 'wb') as fout:
+        fout.write(data)
+    return dest
+
 
 def largest_face(faces):
     selected = 0
@@ -32,6 +45,10 @@ def eyepos(img):
     face = largest_face(faces)
     cv2.rectangle(img, (face.left(), face.top()), (face.right(), face.bottom()), (255, 255, 0), 2)
     return img
+
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(download_predictor())
 
 if __name__ == '__main__':
     cap = cv2.VideoCapture(0)
